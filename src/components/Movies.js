1e5071b057edca421566a6289f5339aa1915a228
Lisa <redlisa125@gmail.com>
2021-11-02 14:01:30 -1000

    commit tree 10a8261f032736b792256bb9ff9331e74b8d342f

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    <h1>Movies Page</h1>
+    <div>
+      <h2>Title: "Doctor Strange"</h2>
+      <p>Time: 115</p>
+      <p>Genres:
+        <ul>
+          <li>Action</li>
+          <li>Adventure</li>
+          <li>Fantasy</li>
+        </ul></p>
+    </div>
+  </div>);
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,38 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Switch } from "react-router-dom";
+import Movies from "./Movies"
+import Directors from "./Directors"
+import Actors from "./Actors"
+import Home from "./Home"
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (<ul>
+
+    <li>
+      <NavLink to="/">
+        Home
+      </NavLink>
+    </li>
+
+    <li>
+      <NavLink to="/movies">
+        Movies
+      </NavLink>
+    </li>
+
+    <li>
+      <NavLink to="/directors">
+        Directors
+      </NavLink>
+    </li>
+
+    <li>
+      <NavLink to="/actors">
+        Actors
+      </NavLink>
+    </li>
+
+  </ul>);
 }
 
 export default NavBar;
